58d67ed1eb13d30fa5c90f486399b88e50a6224e
kapoor273 <heyfarhad.uk@gmail.com>
2025-03-14 14:22:04 +0500

    Add files via upload

---
+++ first.js
@@ -0,0 +1,144 @@
+ //arthematic aparater
+ /*let a = 5;
+let b =2;
+console.log("a+b= ",a+b);
+console.log("a-b=",a-b);
+console.log("a*b=",a*b);
+console.log("a/b=",a/b);
+console.log("a%b=",a%b);
+console.log("a power b = ", a ** b);
+ */
+//*********************//UNARY APARATER//******************************** //
+
+//INCREMENT OPARATER 
+//let i=1;
+//console.log("++i",++i);
+//console.log("++i",i);
+//console.log("--i =",--i);
+//console.log("--i",i);
+//console.log("its farhad programer");
+
+//****************//ASSIGMENT OPPARATER(=,+=,%=,*=,**=,-=)************************//
+
+                  //***************//+=//****************/
+                  
+        //a**=b;
+        //console.log(" a power", a );
+        /*console.log("a=",a,"b=",b);
+        console.log("5==5 is ",a==b);
+        
+        console.log("a!=is ",a!=b);
+        
+        console.log("a>=b is ",a>=b);
+        console.log("a>b is ",a>b);
+        console.log("a<b is ",a<b);
+        console.log("a<=b is ",a<=b);*/
+
+        //let a=5;
+       // let b=5;
+        //console.log("a===b is ",a===b);//===is used to give u the value according to data type
+        
+      //******************((" LOGICAL OPARATOR"))*******************//
+      //1)  AND 0PARATOR (&&)
+      //let a=4;
+      //let b=6;
+      //let cond1=b===a;
+      //let cond2=a===b;
+      //console.log("cond1 && cond2",cond1 && cond2);
+      //console.log("cond1 || cond2",cond1 || cond2);
+      //console.log("!(6==6)",!(6==6));
+
+
+         //*****************// CONITIONAL STATMENTS********************//
+      /*let mood ="bright";
+      if(mood==="dark"){
+        colur = "black";
+      }
+      if(mood === "bright"){
+          colur="wight";
+      }
+      console.log(colur);*/
+      /*age=66;
+      if(age>=20){
+        console.log("you are big boy now");
+      }
+      if((age <20) && (age >10) ){
+        console.log("you are teen ager ");
+      }*/
+            /* let num=8;
+                if(num%2 == 0){
+                  x = 'even';
+                      }else{
+                        x='odd';
+     }    console.log(num,'is',x);
+         let phoneName="sumsing";
+         if(phoneName==="iphone"){
+           so="iphone";
+         }else {
+          so="andride";
+       }console.log(so);*/
+       /*let day = 6;
+       switch(day){
+        case 1:
+          console.log("today is monday");
+          break;
+          case 2:
+            console.log("today is teusday");
+            break;
+            case 4:
+              console.log("todat is friday");
+              break;
+
+          default:
+          console.log("no spacial case");
+
+       }
+      let frindName=prompt("enter your number");
+      switch (frindName){
+
+        case "husain shah":
+          console.log("nationaly margary");
+          break;
+          case "shoaib":
+            console.log("shakee margary");
+            break;
+            case "asad":
+              console.log("tensiony margary");
+              break;
+          default:
+            console.log("i can't write any more");
+      }
+     let num=prompt("enter a num");
+     if(num%5===0){
+      console.log(num,"multiple of five");
+     }else{
+      console.log(num,"it is not the multiple of five");
+     }*/
+   /* let marks=prompt("enter your maarks");
+    if((marks>=80)&&(marks<=100)){
+      console.log("grade 'A' ");
+    } else if((marks>=70)&&(marks<80)){
+      console.log("grade 'b' ");
+    } else if((marks>=60)&&(marks<70)){
+      console.log("grade 'c' ");
+    } else if((marks>=50)&&(marks<60)){
+      console.log("grade 'd' ");
+    }else 
+        console.log("fail");*/
+      let age=prompt("entaer your age");
+      let price;
+      if(age<12){
+        price=5;
+      }else if(age<18){
+        price=10
+      }else if(age<60){
+        price=20;
+      }else{
+        price=25;
+      }
+      console.log(price,"is ticket price according to ur age which is ",age)
+    
+  
+
+    
+          
